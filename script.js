a18f76b38c9cf3aef08dafe82877816720e867c1
Frantisek Pastorek <pastorek@alfabase.sk>
2026-03-13 12:54:18 +0100

    Oprava galerie. Pridanie novej polozky do galerie.

--- script.js
+++ script.js
@@ -68,33 +68,13 @@ if (form && submitBtn && messageBox) {
     });
 }
 
-// GALLERY MODAL SCRIPT
-const galleryImages = [
-    "https://www.audiolux.sk/web/image/425-ec72f893/Galeria%202.webp",
-    "https://www.audiolux.sk/web/image/454-c024d41a/g%2012.webp",
-    "https://www.audiolux.sk/web/image/453-1794b5c3/g%209.webp",
-    "https://www.audiolux.sk/web/image/452-b6ca24d8/g%208.webp",
-    "https://www.audiolux.sk/web/image/448-e8b8411c/g%205.webp",
-    "https://www.audiolux.sk/web/image/449-eb712875/g%2010.webp",
-    "https://www.audiolux.sk/web/image/447-938b2db3/g%2014.webp",
-    "https://www.audiolux.sk/web/image/450-d24c2ab7/g%2011.webp",
-    "https://www.audiolux.sk/web/image/455-38750319/g%2013.webp",
-    "https://www.audiolux.sk/web/image/424-2c97eaf1/Galeria%204.webp",
-    "https://www.audiolux.sk/web/image/426-578a632b/Galeria%201.webp",
-    "https://www.audiolux.sk/web/image/451-1e90474a/g%207.webp",
-    "https://www.audiolux.sk/web/image/427-cb064270/WhatsApp%20Image%202023-12-31%20at%2023.08.02_44c1353b.webp",
-    "https://www.audiolux.sk/web/image/335-81a3182b/307020004_623090175929685_5340014581742383801_n.webp",
-    "https://www.audiolux.sk/web/image/501-b466f8fd/g%2018.webp",
-    "https://www.audiolux.sk/web/image/502-d26aabb7/g%2015.webp",
-    "https://www.audiolux.sk/web/image/503-81300d84/g%2017.webp",
-    "https://www.audiolux.sk/web/image/504-4084a95a/g%2016.webp",
-];
-
-let currentImageIndex = 0;
-
-function openGalleryModal(index) {
-    currentImageIndex = index;
-    document.getElementById("modalImage").src = galleryImages[index];
+function openGalleryModal(event) {
+    const img = event?.target
+    if (!img) {
+        console.error('No image found');
+        return;
+    }
+    document.getElementById("modalImage").src = img.src;
     document.getElementById("galleryModal").style.display = "block";
     document.body.style.overflow = "hidden";
 }
@@ -105,14 +85,18 @@ function closeGalleryModal() {
 }
 
 function changeGalleryImage(direction) {
-    currentImageIndex += direction;
-    if (currentImageIndex < 0) {
-        currentImageIndex = galleryImages.length - 1;
-    } else if (currentImageIndex >= galleryImages.length) {
-        currentImageIndex = 0;
+    const images = Array.from(document.querySelectorAll('#galeria div.gallery-item > img')).map(img => img.src);
+    const currentImageIndex = images.indexOf(document.getElementById("modalImage").src);
+    // left
+    if (direction === -1) {
+        if (currentImageIndex === 0) document.getElementById("modalImage").src = images[images.length - 1];
+        else document.getElementById("modalImage").src = images[currentImageIndex - 1];
     }
-    document.getElementById("modalImage").src =
-        galleryImages[currentImageIndex];
+    // right
+    else if (direction === 1) {
+        if (currentImageIndex === images.length - 1) document.getElementById("modalImage").src = images[0];
+        else document.getElementById("modalImage").src = images[currentImageIndex + 1];
+    }
 }
 
 // Keyboard navigation
